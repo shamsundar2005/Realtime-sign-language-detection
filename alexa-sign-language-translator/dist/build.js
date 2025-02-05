a5a1e34c54193b9245e6162b684e869ffe04534c
shamsundar2005 <shamsundarak2005@gmail.com>
2025-02-05 10:08:25 +0530

    added main.js

--- alexa-sign-language-translator/dist/build.js
+++ alexa-sign-language-translator/dist/build.js
@@ -4,7 +4,7 @@
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 var _deeplearnKnnImageClassifier = require('deeplearn-knn-image-classifier');
-git init
+
 var _deeplearn = require('deeplearn');
 
 var dl = _interopRequireWildcard(_deeplearn);
